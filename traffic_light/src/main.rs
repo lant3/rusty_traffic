60aba2de0d6ea09bf3a45484b67f18576c11e8aa
lant3 <111737324+lant3@users.noreply.github.com>
2026-07-26 19:39:51 +0100

    purple change for deployment test

--- traffic_light/src/main.rs
+++ traffic_light/src/main.rs
@@ -3,7 +3,7 @@ use macroquad::prelude::*;
 #[macroquad::main("Traffic Light")]
 async fn main() {
     loop {
-        clear_background(GREEN);
+        clear_background(PURPLE);
         next_frame().await;
     }
 }
