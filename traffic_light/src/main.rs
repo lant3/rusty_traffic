9aae8422e88b8bd51da204245bcf90fd0b8ddb5d
lant3 <111737324+lant3@users.noreply.github.com>
2026-07-26 07:23:01 +0100

    fix toolchain

--- traffic_light/src/main.rs
+++ traffic_light/src/main.rs
@@ -3,7 +3,7 @@ use macroquad::prelude::*;
 #[macroquad::main("Traffic Light")]
 async fn main() {
     loop {
-        clear_background(GRAY);
+        clear_background(GREEN);
         next_frame().await;
     }
 }
