4ad41818197ae64c0441ff0d7a0162c156bc7275
lant3 <111737324+lant3@users.noreply.github.com>
2026-07-28 07:11:40 +0100

    GREEN

--- traffic_light/src/main.rs
+++ traffic_light/src/main.rs
@@ -3,7 +3,7 @@ use macroquad::prelude::*;
 #[macroquad::main("Traffic Light")]
 async fn main() {
     loop {
-        clear_background(PURPLE);
+        clear_background(GREEN);
         next_frame().await;
     }
 }
