06bc0061a331857fdc40e7750d527ccc9b1133e1
lant3 <111737324+lant3@users.noreply.github.com>
2026-08-02 14:20:54 +0100

    Offset colours and new timer logic

--- traffic_light/src/main.rs
+++ traffic_light/src/main.rs
@@ -17,40 +17,41 @@ impl TrafficLight {
     fn display_color(&self) {
         match self.color {
             TrafficLightState::Red => {
-                draw_circle(screen_width() / 2.0, screen_height() / 2.0, 50.0, RED);
+                draw_circle((screen_width() / 2.0) - 100.0, screen_height() / 2.0, 100.0, RED);
             }
             TrafficLightState::Yellow => {
-                draw_circle(screen_width() / 2.0, screen_height() / 2.0, 50.0, YELLOW);
+                draw_circle(screen_width() / 2.0, screen_height() / 2.0, 100.0, YELLOW);
             }
             TrafficLightState::Green => {
-                draw_circle(screen_width() / 2.0, screen_height() / 2.0, 50.0, GREEN);
+                draw_circle((screen_width() / 2.0) + 100.0, screen_height() / 2.0, 100.0, GREEN);
             }
         }
     }
+    
 
     fn color_change(&mut self) {
         match self.color {
             TrafficLightState::Red => {
                 self.color = TrafficLightState::Yellow;
                 self.previous_color = TrafficLightState::Red;
-                self.timer = 2.0;
+                self.timer = self.timer / 2.0; // Set timer to half of the previous value
             }
             TrafficLightState::Green => {
                 self.color = TrafficLightState::Yellow;
                 self.previous_color = TrafficLightState::Green;
-                self.timer = 2.0;
+                self.timer = self.timer / 2.0;
             }
             TrafficLightState::Yellow => {
                 match self.previous_color {
                     TrafficLightState::Red => {
                         self.color = TrafficLightState::Green;
                         self.previous_color = TrafficLightState::Yellow;
-                        self.timer = 2.0;
+                        self.timer = self.timer;
                     }
                     TrafficLightState::Green => {
                         self.color = TrafficLightState::Red;
                         self.previous_color = TrafficLightState::Yellow;
-                        self.timer = 2.0;
+                        self.timer = self.timer;
                     }
                     TrafficLightState::Yellow => {
                         // This case should not happen, but we can handle it gracefully
@@ -72,14 +73,27 @@ async fn main() {
     let mut traffic_light = TrafficLight {
     color: TrafficLightState::Red,
     previous_color: TrafficLightState::Yellow,
-    timer: 4.0,};
+    timer: 5.0,};
+
+    let timer: f32 = traffic_light.timer;
     
     loop {
-        clear_background(GRAY);
+        clear_background(WHITE);
         let delta = get_frame_time();
+           
+        draw_text(
+    &format!("Isabelle & Jack - Time remaining: {:.2}",
+            traffic_light.timer),
+            screen_width() / 5.0,
+            screen_height() / 10.0,
+            30.0,
+            BLACK
+        );
         traffic_light.display_color();
+
         traffic_light.timer -= delta;
         if traffic_light.timer <= 0.0 {
+            traffic_light.timer = timer; // Reset the timer to the original value
             traffic_light.color_change();
         }
         next_frame().await;
